9acc8db606b0ab94f33a823ae39947750dd65574
Rohith Raj <rohith.raj.gre@gmail.com>
2017-12-18 07:30:20 -0500

    added new programs

--- src/main/java/com/algorithms/arrays/HelperArray.java
+++ src/main/java/com/algorithms/arrays/HelperArray.java
@@ -3,10 +3,17 @@ package com.algorithms.arrays;
 public class HelperArray {
 
     public static void printArray(int[] arr) {
+        System.out.println();
         for(int a : arr) {
             System.out.print(a + " ");
         }
+    }
+
+    public static void printArray(long[] arr) {
         System.out.println();
+        for(long a : arr) {
+            System.out.print(a + " ");
+        }
     }
 
     public static void printArray(char[] ch) {
@@ -14,5 +21,19 @@ public class HelperArray {
             System.out.print(c + " ");
         }
         System.out.println();
+    }
+
+    public static void printArray(int[] arr, int startIndex, int endIndex) {
+        System.out.println();
+        for(int i = startIndex; i < endIndex; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void printArray(long[] arr, int startIndex, int endIndex) {
+        System.out.println();
+        for(int i = startIndex; i < endIndex; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
---
+++ src/main/java/com/algorithms/arrays/arrangements_rearrangements/Segregate.java
@@ -0,0 +1,36 @@
+package com.algorithms.arrays.arrangements_rearrangements;
+
+import com.algorithms.arrays.HelperArray;
+
+public class Segregate {
+
+    private static void segregate(int[] arr, int size) {
+        if (size == 0 || size == 1) {
+            return;
+        }
+
+        int left = 0, right = size - 1;
+
+        while (left < right) {
+            while (arr[left] == 0 && left < right) {
+                left++;
+            }
+
+            while (arr[right] == 1 && left < right) {
+                right--;
+            }
+            if (arr[left] == 1 && arr[right] == 0) {
+                arr[left] = 0;
+                arr[right] = 1;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 0, 1, 1, 1};
+        segregate(arr, arr.length);
+        HelperArray.printArray(arr);
+    }
+}
